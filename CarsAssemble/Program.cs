73202deee2b50966eb488266a8c66057aa194089
nick27liberatto <nicolas.liberatto@gmail.com>
2025-03-02 18:35:21 -0300

    CarsAssemble Initial Commit

---
+++ CarsAssemble/Program.cs
@@ -0,0 +1,40 @@
+﻿static class AssemblyLine
+{
+    public static double SuccessRate(int speed)
+    {
+        if (speed == 0)
+        {
+            return 0.0;
+        } 
+        else if ((speed => 1) && (speed <= 4))
+        {
+            return 0.100;
+        } 
+        else if ((speed =>5) && (speed <=8))
+        {
+            return 0.90;
+        }
+        else if (speed == 9)
+        {
+            return 0.80;
+        } else if (speed == 10)
+        {
+            return 0.77;
+        }
+        return speed;
+    }
+    
+    // public static double ProductionRatePerHour(int speed)
+    // {
+    //     throw new NotImplementedException("Please implement the (static) AssemblyLine.ProductionRatePerHour() method");
+    // }
+
+    // public static int WorkingItemsPerMinute(int speed)
+    // {
+    //     throw new NotImplementedException("Please implement the (static) AssemblyLine.WorkingItemsPerMinute() method");
+    // }
+    public static void Main()
+    {
+        Console.WriteLine(SuccessRate(10));
+    }
+}
