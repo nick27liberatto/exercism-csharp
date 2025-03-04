bc912cbf4a88da0ed08dbc2e4f2ad1c4a3392176
nick27liberatto <nicolas.liberatto@gmail.com>
2025-03-03 21:05:49 -0300

    update cars assemble

--- CarsAssemble/Program.cs
+++ CarsAssemble/Program.cs
@@ -2,37 +2,40 @@
 {
     public static double SuccessRate(int speed)
     {
+        double ratio = 0.0;
         if (speed == 0)
         {
-            return 0.0;
+            return ratio;
         } 
-        else if ((speed => 1) && (speed <= 4))
+        else if ((speed > 0) && (speed <= 4))
         {
-            return 0.100;
+            ratio = 1;
         } 
-        else if ((speed =>5) && (speed <=8))
+        else if ((speed >4 ) && (speed <=8))
         {
-            return 0.90;
+            ratio = 0.90;
         }
         else if (speed == 9)
         {
-            return 0.80;
+            ratio = 0.80;
         } else if (speed == 10)
         {
-            return 0.77;
+            ratio = 0.77;
         }
-        return speed;
+        return ratio;
     }
     
-    // public static double ProductionRatePerHour(int speed)
-    // {
-    //     throw new NotImplementedException("Please implement the (static) AssemblyLine.ProductionRatePerHour() method");
-    // }
+    public static double ProductionRatePerHour(int speed)
+    {
+        double ratio = SuccessRate(speed);
+        double rate = speed * 221;
+        return ratio * rate;
+    }
 
-    // public static int WorkingItemsPerMinute(int speed)
-    // {
-    //     throw new NotImplementedException("Please implement the (static) AssemblyLine.WorkingItemsPerMinute() method");
-    // }
+    public static int WorkingItemsPerMinute(int speed)
+    {
+        throw new NotImplementedException("Please implement the (static) AssemblyLine.WorkingItemsPerMinute() method");
+    }
     public static void Main()
     {
         Console.WriteLine(SuccessRate(10));
